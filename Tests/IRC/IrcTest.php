e25cd765621304df3aed1174b2c16eebaf6d82b1
Fred Akers <knivey@botops.net>
2014-05-13 08:08:55 -0500

    Clean things up
    Getting ready for more testing and bringing the projects code into the future

--- Tests/IRC/IrcTest.php
+++ Tests/IRC/IrcTest.php
@@ -26,6 +26,36 @@ class IrcTest extends PHPUnit_Framework_TestCase {
         self::$Irc = new Irc(self::$sockets, 'UnitBot', 0, 'localhost', 4);
     }
     
+    public function testConstruct() {
+        $irc = new Irc(self::$sockets, 'UnitBot', '127.0.0.1', 'testserv', 4, 1234, 'unitpass', 50, 90);
+        
+        $this->assertAttributeEquals(
+            'bots localhost localhost :IRC Bot Services #Bots',
+            'user', $irc);
+        $this->assertAttributeEquals('testserv', 'server', $irc);
+        $this->assertAttributeEquals(4, 'ipv', $irc);
+        $this->assertAttributeEquals(1234, 'port', $irc);
+        $this->assertAttributeEquals('unitpass', 'pass', $irc);
+        $this->assertAttributeEquals(50, 'timeout', $irc);
+        $this->assertAttributeEquals('127.0.0.1', 'bind', $irc);
+        $this->assertAttributeEquals('UnitBot', 'nick', $irc);
+        $this->assertAttributeEquals(self::$sockets, 'pSockets', $irc);
+        $this->assertAttributeEquals(90, 'RTO', $irc);
+        $this->assertAttributeInstanceOf('IrcFilters', 'ircFilters', $irc);
+        $this->assertAttributeGreaterThan(time()+8, 'nickCheck', $irc);
+        $this->assertAttributeInstanceOf('Nicks', 'Nicks', $irc);
+        $this->assertAttributeInstanceOf('KEventServer', 'eventServer', $irc);
+        
+        $irc = new Irc(self::$sockets, 'UnitBot', '127.0.0.1', 'testserv', 4);
+        $this->assertAttributeEquals(6667, 'port', $irc);
+        $this->assertAttributeEquals('', 'pass', $irc);
+        $this->assertAttributeEquals(30, 'timeout', $irc);
+        $this->assertAttributeEquals(170, 'RTO', $irc);
+        
+        $irc = new Irc(self::$sockets, 'UnitBot', '127.0.0.1', 'testserv', 8);
+        $this->assertAttributeEquals(4, 'ipv', $irc);
+    }
+    
     public function testKillBot() {
         //just putting something here for now
         $this->assertNull(self::$Irc->killBot('testing'));
