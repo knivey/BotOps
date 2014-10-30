8830289a0564c5fde37fde791d2380736fb3fa3e
Fred Akers <knivey@botops.net>
2014-10-30 18:43:55 -0500

    Update castinfo for tracking streams

--- BotOps/modules/castinfo/registry.php
+++ BotOps/modules/castinfo/registry.php
@@ -10,9 +10,13 @@ class registry {
     var $CmdReg = Array(
         'funcs' => Array(
             Array('castinfo', 'cmd_castinfo', "<ip:port>", "Get shoutcast or IceCast info"),
+            Array('casttrack', 'cmd_casttrack', "<ip:port>", "Connect to a stream and update on changes."),
+            Array('caststop', 'cmd_caststop', "<ip:port>", "Stop a casttrack"),
         ),
         'binds' => Array(
 			Array('castinfo', '0', 'castinfo', "", "", "", '0'),
+            Array('casttrack', '0', 'casttrack', "", "", "", '0'),
+            Array('caststop', '0', 'caststop', "", "", "", '0'),
         )
     );
 }
