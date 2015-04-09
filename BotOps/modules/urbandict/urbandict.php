8135fe1f3ebedea1cc081147c86d3034fcaf72b2
Fred Akers <knivey@botops.net>
2015-04-09 04:47:40 -0400

    Silence warning, format source

--- BotOps/modules/urbandict/urbandict.php
+++ BotOps/modules/urbandict/urbandict.php
@@ -19,58 +19,64 @@ class urbandict extends Module
         }
 
         $lol = new Http($this->pSockets, $this, 'ud');
-        $lol->getQuery('http://www.urbandictionary.com/define.php?term=' . urlencode(htmlentities($msg)), $chan);
+        $lol->getQuery('http://www.urbandictionary.com/define.php?term=' . urlencode(htmlentities($msg)),
+                                                                                                  $chan);
     }
 
     function ud($data, $chan)
     {
         if (is_array($data)) {
-            $this->pIrc->msg($chan, "\2UrbanDictionary:\2 Error ($data[0]) $data[1]");
+            $this->pIrc->msg($chan,
+                             "\2UrbanDictionary:\2 Error ($data[0]) $data[1]");
             return;
         }
 
         if (strpos($data, "</i> isn't defined ") !== FALSE) {
-            $this->pIrc->msg($chan, "\2UrbanDict:\2 Your query hasn't been defined yet.", 1, 1);
+            $this->pIrc->msg($chan,
+                             "\2UrbanDict:\2 Your query hasn't been defined yet.",
+                             1, 1);
             return;
         }
 
         $doc = str_get_html($data);
 
-        $word    = $doc->find('a.word')[0]->plaintext;
-        
-        if(!$word) {
-            $this->pIrc->msg($chan, "\2UrbanDict:\2 Your query hasn't been defined yet.", 1, 1);
+        $word = @$doc->find('a.word')[0]->plaintext;
+
+        if (!$word) {
+            $this->pIrc->msg($chan,
+                             "\2UrbanDict:\2 Your query hasn't been defined yet.",
+                             1, 1);
             return;
         }
-        
-        $by      = $doc->find('div.contributor')[0]->plaintext;
-        $meaning = $doc->find('div.meaning')[0]->plaintext;
-        $example = $doc->find('div.example')[0]->plaintext;
+
+        $by       = $doc->find('div.contributor')[0]->plaintext;
+        $meaning  = $doc->find('div.meaning')[0]->plaintext;
+        $example  = $doc->find('div.example')[0]->plaintext;
         $relatedA = $doc->find('ul.no-bullet>.tag');
 
         $meaning = html_entity_decode($meaning, ENT_QUOTES);
         $example = html_entity_decode($example, ENT_QUOTES);
         $word    = html_entity_decode($word, ENT_QUOTES);
         $by      = html_entity_decode($by, ENT_QUOTES);
-        
+
         $related = Array();
-        $cnt = 0;
-        foreach($relatedA as $e) {
+        $cnt     = 0;
+        foreach ($relatedA as $e) {
             $related[] = trim(html_entity_decode($e->plaintext, ENT_QUOTES));
             $cnt++;
-            if($cnt > 5) {
+            if ($cnt > 5) {
                 break;
             }
         }
         $related = implode(", ", $related);
-        
+
         $related = html_entity_decode($related, ENT_QUOTES);
-        
+
         $by = preg_replace("/^ by/", "\2By:\2", $by);
 
         $meaning = str_replace("\n", ' ', $meaning);
         $meaning = trim(str_replace("\r", ' ', $meaning));
-        
+
         $example = str_replace("\r", "\n", $example);
         $example = explode("\n", $example);
         foreach ($example as $key => &$ex) {
