7dc4a5fc865b38d03a254a691daf37dbf94965d9
Fred Akers <knivey@botops.net>
2015-04-12 02:15:40 -0500

    Quick source formatting and cleanup

--- BotOps/modules/clanbot/clanbot.php
+++ BotOps/modules/clanbot/clanbot.php
@@ -1,106 +1,106 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
+class clanbot extends Module
+{
 
-class clanbot extends Module {
-    
-    /*
+    /**
      * checks if our settings exist on the channel
      * if not then create them
      */
-    function checkInit($chan) {
+    function checkInit($chan)
+    {
         $sets = $this->gM('channel')->getSet($chan, 'clanbot', 'binds');
-        if($sets != null) {
+        if ($sets != null) {
             return;
         }
         $sets = Array();
         $this->gM('channel')->chgSet($chan, 'clanbot', 'binds', $sets);
     }
-    
-    function getBind($chan, $bind) {
+
+    function getBind($chan, $bind)
+    {
+        $chan = strtolower($chan);
         $bind = strtolower($bind);
         $this->checkInit($chan);
         $sets = $this->gM('channel')->getSet($chan, 'clanbot', 'binds');
-        if(!is_array($sets) || !array_key_exists($bind, $sets)) {
+        if (!is_array($sets) || !array_key_exists($bind, $sets)) {
             return null;
         } else {
             return $sets[$bind];
         }
     }
-    
-    function getAllBinds($chan) {
+
+    function getAllBinds($chan)
+    {
         $this->checkInit($chan);
         return $this->gM('channel')->getSet($chan, 'clanbot', 'binds');
     }
-    
-    function setBind($chan, $bind, $data) {
-        $bind = strtolower($bind);
+
+    function setBind($chan, $bind, $data)
+    {
+        $bind        = strtolower($bind);
         $this->checkInit($chan);
-        $sets = $this->gM('channel')->getSet($chan, 'clanbot', 'binds');
+        $sets        = $this->gM('channel')->getSet($chan, 'clanbot', 'binds');
         $sets[$bind] = $data;
         $this->gM('channel')->chgSet($chan, 'clanbot', 'binds', $sets);
     }
-    
-    function delBind($chan, $bind) {
+
+    function delBind($chan, $bind)
+    {
         $bind = strtolower($bind);
         $this->checkInit($chan);
         $sets = $this->gM('channel')->getSet($chan, 'clanbot', 'binds');
         unset($sets[$bind]);
         $this->gM('channel')->chgSet($chan, 'clanbot', 'binds', $sets);
     }
-    
-    function cmd_bind($nick, $target, $arg2) {
-        $arg = explode(' ', $arg2);
+
+    function cmd_bind($nick, $target, $arg2)
+    {
+        $arg  = explode(' ', $arg2);
         $host = $this->pIrc->n2h($nick);
         $hand = $this->gM('user')->byHost($host);
         $chan = strtolower($target);
-        if(count($arg) < 2) {
+        if (count($arg) < 2) {
             return $this->BADARGS;
         }
-        $bind = array_shift($arg);
-        $value = implode(' ', $arg);
+        $bind    = array_shift($arg);
+        $value   = implode(' ', $arg);
         //check if the bind exists
         $newBind = $this->getBind($chan, $bind);
-        if($newBind == null) {
+        if ($newBind == null) {
             $newBind = Array(
                 'value' => $value,
-                'name' => $bind,
-                'by' => $hand,
-                'date' => time(),
-                'type' => 'default',
+                'name'  => $bind,
+                'by'    => $hand,
+                'date'  => time(),
+                'type'  => 'default',
                 'count' => 0
             );
-            $reply = "Bind $bind has been added";
+            $reply   = "Bind $bind has been added";
         } else {
-            $newBind['name'] = $bind;
+            $newBind['name']  = $bind;
             $newBind['value'] = $value;
-            $newBind['by'] = $hand;
-            $newBind['date'] = time();
-            $reply = "Bind $bind has been updated";
+            $newBind['by']    = $hand;
+            $newBind['date']  = time();
+            $reply            = "Bind $bind has been updated";
         }
         $this->setBind($chan, $bind, $newBind);
         $this->pIrc->notice($nick, $reply);
     }
-    
+
     public $bindvalue = '';
-    
-    function v_bindvalue() {
+
+    function v_bindvalue()
+    {
         $bv = $this->bindvalue;
         unset($this->bindvalue);
         return $bv;
     }
-    
-    function cmdOut($message, $extras) {
-        //$extras = Array($cmd, $nick, $chan, $bindInfo, $type);
-        $cmd = $extras[0];
-        $nick = $extras[1];
-        $chan = $extras[2];
-        $bindInfo = $extras[3];
-        $type = $extras[4];
-        switch($type) {
+
+    function cmdOut($message, $extras)
+    {
+        list($cmd, $nick, $chan, $bindInfo, $type) = $extras;
+        switch ($type) {
             default:
             case 'notice':
                 $this->pIrc->notice($nick, $message);
@@ -112,23 +112,25 @@ class clanbot extends Module {
                 $this->pIrc->msg($chan, $message);
                 break;
         }
-        $bindInfo['count']++;
+        $bindInfo['count'] ++;
         $this->setBind($chan, $cmd, $bindInfo);
     }
-    
-    function cmdCatch($cmd, $nick, $chan, $arg2) {
+
+    function cmdCatch($cmd, $nick, $chan, $arg2)
+    {
         $bindInfo = $this->getBind($chan, $cmd);
-        if($bindInfo == null) {
+        if ($bindInfo == null) {
             return false;
         }
-        $type = strtolower($bindInfo['type']);
+        $type            = strtolower($bindInfo['type']);
         $this->bindvalue = $bindInfo['value'];
-        $theme = $this->gM('SetReg')->getCSet('clanbot', $chan, 'theme');
-        if($type == 'default') {
+        $theme           = $this->gM('SetReg')->getCSet('clanbot', $chan,
+                                                        'theme');
+        if ($type == 'default') {
             $type = $this->gM('SetReg')->getCSet('clanbot', $chan, 'bindtype');
         }
         $this->gM('CmdReg')->lastCmdInfo['cmd'] = $bindInfo['name'];
-        if($type == 'act') {
+        if ($type == 'act') {
             $this->pIrc->act($chan, $bindInfo['value']);
             return true;
         }
@@ -136,93 +138,98 @@ class clanbot extends Module {
         $this->gM('ParseUtil')->parse($theme, 'cmdOut', $this, $extras);
         return true;
     }
-    
-    function cmd_unbind($nick, $target, $arg2) {
-        $arg = explode(' ', $arg2);
-        $host = $this->pIrc->n2h($nick);
-        $hand = $this->gM('user')->byHost($host);
+
+    function cmd_unbind($nick, $target, $arg2)
+    {
+        $arg  = explode(' ', $arg2);
         $chan = strtolower($target);
-        if($arg2 == '') {
+        if ($arg2 == '') {
             return $this->BADARGS;
         }
         $bindInfo = $this->getBind($chan, $arg[0]);
-        if($bindInfo == null) {
+        if ($bindInfo == null) {
             $this->pIrc->notice($nick, "Bind $arg[0] does not exist");
             return $this->ERROR;
         }
         $this->delBind($chan, $arg[0]);
         $this->pIrc->notice($nick, "Bind $arg[0] has been removed");
     }
-    
-    function cmd_bindtype($nick, $target, $arg2) {
-        $arg = explode(' ', $arg2);
-        $host = $this->pIrc->n2h($nick);
-        $hand = $this->gM('user')->byHost($host);
+
+    function cmd_bindtype($nick, $target, $arg2)
+    {
+        $arg  = explode(' ', $arg2);
+        $hand = $this->gM('user')->byNick($nick);
         $chan = strtolower($target);
-        if($arg2 == '') {
+        if ($arg2 == '') {
             return $this->BADARGS;
         }
         $bindInfo = $this->getBind($chan, $arg[0]);
-        if($bindInfo == null) {
+        if ($bindInfo == null) {
             $this->pIrc->notice($nick, "Bind $arg[0] does not exist");
             return $this->ERROR;
         }
-        if(!isset($arg[1])) {
+        if (!isset($arg[1])) {
             $this->pIrc->notice($nick, "Bindtype for $arg[0] is $bindInfo[type]");
             return $this->OK;
         }
         $type = strtolower($arg[1]);
-        if($type != 'default' && $type != 'notice' && $type != 'chan' && $type != 'act') {
-            $this->pIrc->notice($nick, "Bind please choose a correct type: default, notice, chan, act");
+        if ($type != 'default' && $type != 'notice' && $type != 'chan' && $type != 'act') {
+            $this->pIrc->notice($nick,
+                                "Bind please choose a correct type: default, notice, chan, act");
             return $this->ERROR;
         }
         $bindInfo['type'] = $type;
-        $bindInfo['by'] = $hand;
+        $bindInfo['by']   = $hand;
         $bindInfo['date'] = time();
         $this->setBind($chan, strtolower($arg[0]), $bindInfo);
         $this->pIrc->notice($nick, "Bindtype for $arg[0] is now set to $type");
     }
-    
-    function v_binds() {
-        $chan = $this->gM('ParseUtil')->getV('chan');
+
+    function v_binds()
+    {
+        $chan  = $this->gM('ParseUtil')->getV('chan');
         $binds = $this->getAllBinds($chan);
-        $list = implode(', ', array_keys($binds));
+        $list  = implode(', ', array_keys($binds));
         return $list;
     }
-    
-    function v_tbinds() {
-        $chan = $this->gM('ParseUtil')->getV('chan');
+
+    function v_tbinds()
+    {
+        $chan  = $this->gM('ParseUtil')->getV('chan');
         $binds = $this->getAllBinds($chan);
-        $trig = $this->gM('channel')->getTrig($chan);
-        $list = '';
-        foreach(array_keys($binds) as $b) {
+        $trig  = $this->gM('channel')->getTrig($chan);
+        $list  = '';
+        foreach (array_keys($binds) as $b) {
             $list .= $trig . $b . ' ';
         }
         return trim($list);
     }
-    
-    function cmd_binds($nick, $chan, $arg2) {
+
+    function cmd_binds($nick, $chan, $arg2)
+    {
         $binds = $this->getAllBinds($chan);
-        $list = implode(', ', array_keys($binds));
+        $list  = implode(', ', array_keys($binds));
         $this->pIrc->notice($nick, "Binds: $list");
     }
-    
-    function cmd_bindinfo($nick, $target, $arg2) {
+
+    function cmd_bindinfo($nick, $chan, $arg2)
+    {
         $arg = explode(' ', $arg2);
-        $host = $this->pIrc->n2h($nick);
-        $hand = $this->gM('user')->byHost($host);
-        $chan = strtolower($target);
-        if($arg2 == '') {
+        if ($arg2 == '') {
             return $this->BADARGS;
         }
         $bindInfo = $this->getBind($chan, $arg[0]);
-        if($bindInfo == null) {
+        if ($bindInfo == null) {
             $this->pIrc->notice($nick, "Bind $arg[0] does not exist");
             return $this->ERROR;
         }
-        $this->pIrc->notice($nick, "Bind $bindInfo[name] of type $bindInfo[type] last changed " . strftime('%D', $bindInfo['date']) . " by $bindInfo[by] and has been used $bindInfo[count] times Value: $bindInfo[value]",1,1);
+        $this->pIrc->notice($nick,
+                            "Bind $bindInfo[name] of type $bindInfo[type]" .
+            " last changed " . strftime('%D', $bindInfo['date']) .
+            " by $bindInfo[by] and has been used $bindInfo[count] times" .
+            " Value: $bindInfo[value]", 1, 1);
     }
-    
+
 }
 
 ?>
